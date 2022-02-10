c10412b8abbe6ac2e84172a938f7ad0710ba6c77
Nikita Luksha <nikita.luksha@softensity.com>
2022-02-10 10:14:21 +0300

    Add DiscountModel

---
+++ OrderSvc-Consumer/OrderSvc-Consumer/DiscountModel.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderSvc_Consumer
+{
+    public class DiscountModel
+    {
+        public double CustomerRaiting { get; set; }
+        public double AmountToDiscount { get; set; }
+    }
+}
