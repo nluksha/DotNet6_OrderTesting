dde8d07c563def7012fe2a774b76783dae2db6f3
Nikita Luksha <nikita.luksha@softensity.com>
2022-02-10 11:02:43 +0300

    Add Mock Tests

---
+++ OrderSvc-Consumer/OrderSvc-Test/DiscountSvcMock.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using PactNet;
+using PactNet.Mocks;
+using PactNet.Mocks.MockHttpService;
+
+namespace OrderSvc_Test
+{
+    public class DiscountSvcMock : IDisposable
+    {
+        private readonly IPactBuilder pactBuilder;
+        private readonly int servicePort = 9222;
+        private bool disposed = false;
+
+        public IMockProviderService MockProviderService { get; }
+        public string ServiceUri => $"http://localhost:{servicePort}";
+
+        public DiscountSvcMock()
+        {
+            var pactConfig = new PactConfig
+            {
+                SpecificationVersion = "2.0.0",
+                PactDir = @"c:/temp/OrderSvcConsumer",
+                LogDir = @"c:/temp/OrderSvcConsumer/logs",
+            };
+
+            pactBuilder = new PactBuilder(pactConfig)
+                .ServiceConsumer("Orders")
+                .HasPactWith("Discounts");
+
+            MockProviderService = pactBuilder.MockService(servicePort,
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                    NullValueHandling = NullValueHandling.Ignore
+                });
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    pactBuilder.Build();
+                }
+
+                disposed = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~DiscountSvcMock()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ OrderSvc-Consumer/OrderSvc-Test/DiscountSvcTests.cs
@@ -0,0 +1,70 @@
+﻿using OrderSvc_Consumer;
+using PactNet.Mocks.MockHttpService;
+using PactNet.Mocks.MockHttpService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OrderSvc_Test
+{
+    public class DiscountSvcTests: IClassFixture<DiscountSvcMock>
+    {
+        private readonly IMockProviderService mockProviderService;
+        private readonly string serviceUri;
+
+        public DiscountSvcTests(DiscountSvcMock discountSvcMock)
+        {
+            mockProviderService = discountSvcMock.MockProviderService;
+            serviceUri = discountSvcMock.ServiceUri;
+            mockProviderService.ClearInteractions();
+        }
+
+        [Fact]
+        public async Task GetDiscountAdjustmentAmount()
+        {
+            var discountModel = new DiscountModel
+            {
+                CustomerRaiting = 4.1
+            };
+
+            mockProviderService
+                .Given("Rate")
+                .UponReceiving("Given a customer rating, a adjustment discount amount will be return.")
+                .With(new ProviderServiceRequest
+                {
+                    Method = HttpVerb.Post,
+                    Path = "/discount",
+                    Body = discountModel,
+                    Headers = new Dictionary<string, object>
+                    {
+                        {"Content-Type", "application/json; charset=utf-8"}
+                    }
+                })
+                .WillRespondWith(new ProviderServiceResponse
+                {
+                    Status = 200,
+                    Headers = new Dictionary<string, object>
+                    {
+                        {"Content-Type", "application/json; charset=utf-8"}
+                    },
+                    Body = new DiscountModel
+                    {
+                        CustomerRaiting = 4.1,
+                        AmountToDiscount = .41
+                    }
+                });
+
+            var httpClient = new HttpClient();
+            var response = await httpClient
+                .PostAsJsonAsync($"{serviceUri}/discount", discountModel);
+            var discountModelReturned = await response.Content.ReadFromJsonAsync<DiscountModel>();
+
+            Assert.Equal(discountModelReturned.CustomerRaiting, discountModelReturned.CustomerRaiting);
+        }
+    }
+}
